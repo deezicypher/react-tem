23930f139f59566a1f530f9db3451262cf5e2b93
deezitheviper <amuhdavid@gmail.com>
2022-12-03 13:51:19 +0100

    status update

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import button from './Button'
+import styles from '../style'
+import Button from './Button';
+
+const {flexCenter,heading2,paragraph, marginY, padding} = styles;
+
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+   <section className={`${flexCenter} ${marginY} ${padding} 
+   sm:flex-row flex-col bg-black-gradient rounded-[20px] box-shadow`}>
+    <div className='flex flex-col flex-1'>
+      <h2 className={`${heading2}`}>Let's try our service now</h2>
+      <p className={`${paragraph} max-w-[580px] mt-5`}>Everything you need to accept both fiat and crypto for both 
+        your business and crypto anywhere in the world</p>
+    </div>
+    <div className={`${flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+   </section>
   )
 }
 
